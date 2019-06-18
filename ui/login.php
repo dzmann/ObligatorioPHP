5bba290997b4cf8de51db6808e01d9846cbdb8a6
Danilo <zimer2627@gmail.com>
2019-06-18 20:33:20 -0300

    added login form

---
+++ ui/login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+    <form action="login.php" method="post">
+        <fieldset>
+            Email<input type="text" name="email" id="email"><br>
+            Contraseña<input type="password" name="contrasenia" id="contrasenia"><br>
+            <input type="submit" value="Entrar" name="entrar">
+        </fieldset>
+     </form>
+    
+</body>
+</html>
