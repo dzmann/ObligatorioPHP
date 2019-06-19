d3fa421096cdc12152aaa3a7b7a69fe769d16d2e
Danilo <zimer2627@gmail.com>
2019-06-18 21:50:51 -0300

    login fix

--- controllers/login.php
+++ controllers/login.php
@@ -2,20 +2,23 @@
 
 require_once("../database/DatabaseOperations.php");
 
-if(isset($_POST["enviar"])){
+if(isset($_POST["entrar"])){
 
     $email = $_POST["email"];
     $contrasenia = $_POST["contrasenia"];
     $rol = $_POST["rol"];
-
+    $contrasenia_md5 = md5($contrasenia);
     $dbOperation = new DatabaseOperations();
 
     if($rol=="encargado"){
         $result = $dbOperation->select("ENCARGADOS", "EMAIL='$email'", array("EMAIL", "NOMBRE", "CONTRASENIA"));
         if(isset($result[0])){
-
-            if($result[0]["EMAIL"]==$email && $result[0]["CONTRASENIA"==$contrasenia]){
-
+            echo "contrasenia: $contrasenia_md5";
+            
+            if($result[0]["EMAIL"]==$email && $result[0]["CONTRASENIA"==$contrasenia_md5]){
+                echo "login correcto";
+            }else{
+                echo "login incorrecto";
             }
 
         }else{
