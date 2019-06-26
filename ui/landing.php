2590c496e26fad0b8f718bccfc37123f138a2f77
DamiÃan <rdamian3@gmail.com>
2019-06-25 21:03:19 -0300

    login WIP

--- ui/landing.php
+++ ui/landing.php
@@ -1,6 +1,9 @@
 <!-- TODO: Landing con los dos perfiles -->
 <div class="main">
     <div class="container">
+        <!-- Llamar método para loguearse - si está logueado mandarlo al mail -->
+
+
         <div class="alumno">
             <a href="ui/main.php">Accesso Alumno</a>
         </div>
---
+++ ui/loginalumno.php
@@ -0,0 +1,7 @@
+<form action="../controllers/login.php" method="post">
+    <fieldset>
+        PIN<input type="text" name="email" id="email"><br>
+        Contraseña<input type="password" name="contrasenia" id="contrasenia"><br>
+        <input type="submit" value="Inscribirme" name="entrar">
+    </fieldset>
+</form>
