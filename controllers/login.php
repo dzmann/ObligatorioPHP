07b3e7f0e69a28380cb274a40225f1c46f5b53b2
Danilo <zimer2627@gmail.com>
2019-06-27 20:02:32 -0300

    added session creation

--- controllers/login.php
+++ controllers/login.php
@@ -2,6 +2,7 @@
 
 require_once("../database/DatabaseOperations.php");
 require_once("../controllers/SessionManager.php");
+require_once("../utils/EntitiesClassLoader.php");
 
 class Login{
 
@@ -11,12 +12,16 @@ class Login{
         $dbOperation = new DatabaseOperations();
         if($rol=="encargado"){
             
-            $result = $dbOperation->select("ENCARGADOS", "EMAIL='$user'", array("EMAIL", "CONTRASENIA"));
+            $result = $dbOperation->select("ENCARGADOS", "EMAIL='$user'", array("NOMBRE","EMAIL", "CONTRASENIA"));
             
             if(isset($result[0])){
                 
                 if($result[0]["EMAIL"]==$user && $result[0]["CONTRASENIA"]==$password_md5){
                     $isLogged=true;
+                    $encargado = new Encargado($result[0]["EMAIL"],$result[0]["NOMBRE"], $result[0]["CONTRASENIA"]);
+                    $sessionManager = new SessionManager();
+                    $sessionManager->createSession($encargado->get("email"), $encargado->get("nombre"));
+        
                 }
             }
 
