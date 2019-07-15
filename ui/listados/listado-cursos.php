5715109182b67de8c49811a5b3e90682a823ae9b
DamiÃan <rdamian3@gmail.com>
2019-07-15 17:42:36 -0300

    arreglado el listado de cursos

--- ui/listados/listado-cursos.php
+++ ui/listados/listado-cursos.php
@@ -20,29 +20,27 @@
         <th>Profesor</th>
         <th>Inscribirse</th>
     </tr>
-    <?php } ?>
-    <tr>
-        <?php
-        require_once "./../utils/Constants.php";
-        require_once PROJECT_ROOT."/controllers/CursosController.php";
+    <?php } 
+    require_once "./../utils/Constants.php";
+    require_once PROJECT_ROOT."/controllers/CursosController.php";
 
-        $CursosController = new CursosController();
-        
-        if($isAdmin){
-            $cursos = $CursosController->getCursosConProfesores();
-            foreach ($cursos as $row) {
-                echo "<tr><td>" . $row['ID'] . "</td><td>" . $row['MATERIA'] . "</td><td>" . $row['NOMBRE_PROFESOR'] ." ". $row['APELLIDO']."
-             <td><a href='seccionEncargado.php?mode=edit&section=addEditCurso&idCurso=" . $row['ID'] . "'><img height='30' width='30' src='./../img/editicon.jpg'></a></td><td><a href=# onclick=borrar('./../controllers/DeleteObj.php?section=cursos&id=".$row['ID']."')><img height='30' width='30' src='./../img/deleteicon.jpg'></a></td></tr>";
-            }    
-        }else{
-            $cursos = $CursosController->getCursos();
+    $CursosController = new CursosController();
+    
+    if($isAdmin){
+        $cursos = $CursosController->getCursosConProfesores();
+        foreach ($cursos as $row) {
+            echo "<tr><td>" . $row['ID'] . "</td><td>" . $row['MATERIA'] . "</td><td>" . $row['NOMBRE_PROFESOR'] ." ". $row['APELLIDO']."
+            <td><a href='seccionEncargado.php?mode=edit&section=addEditCurso&idCurso=" . $row['ID'] . "'><img height='30' width='30' src='./../img/editicon.jpg'></a></td><td><a href=# onclick=borrar('./../controllers/DeleteObj.php?section=cursos&id=".$row['ID']."')><img height='30' width='30' src='./../img/deleteicon.jpg'></a></td></tr>";
+        }    
+    }else{
+        $cursos = $CursosController->getCursos();
 
-            foreach ($cursos as $row) {
-                echo "<tr><td>" . $row['id'] . "</td><td>" . $row['materia'] . "</td><td>" . $row['profesor'] . "</td><td><a href='loginalumno.php?idCurso=" . $row['id'] . "'>Inscribirme</a></td></tr>";
-            }
+        foreach ($cursos as $row) {
+            echo "<tr><td>" . $row['id'] . "</td><td>" . $row['materia'] . "</td><td>" . $row['profesor'] . "</td><td><a href='loginalumno.php?idCurso=" . $row['id'] . "'>Inscribirme</a></td></tr>";
         }
-        
+    }
+    
 
-        ?>
-    </tr>
+    ?>
+
 </table>
